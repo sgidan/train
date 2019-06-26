b3ecfe2a1e97d71015d0550441d70c8276b459ac
sgidan <sarahgidan@gmail.com>
2019-06-26 16:14:55 -0700

    time logic complete

--- java.js
+++ java.js
@@ -1,57 +1,86 @@
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyDeQzA0lijlZUSw4QPXk43woeWZTx0He2M",
-        authDomain: "project-2f099.firebaseapp.com",
-        databaseURL: "https://project-2f099.firebaseio.com",
-        projectId: "project-2f099",
-        storageBucket: "project-2f099.appspot.com",
-        messagingSenderId: "632750689783",
-        appId: "1:632750689783:web:ef4416102b20a1c2"
-      };
+var trainName = "";
+var destination = "";
+var firstTrain = "00:00";
+var frequency = "";
 
-    firebase.initializeApp(firebaseConfig);
 
-    var database = firebase.database();
+var firebaseConfig = {
+    apiKey: "AIzaSyDeQzA0lijlZUSw4QPXk43woeWZTx0He2M",
+    authDomain: "project-2f099.firebaseapp.com",
+    databaseURL: "https://project-2f099.firebaseio.com",
+    projectId: "project-2f099",
+    storageBucket: "project-2f099.appspot.com",
+    messagingSenderId: "632750689783",
+    appId: "1:632750689783:web:ef4416102b20a1c2"
+};
 
-    var trainName = "";
-    var destination = "";
-    var firstTrain = "00:00";
-    var frequency = "";
+firebase.initializeApp(firebaseConfig);
 
-    $("#submit").on("click", function () {
-        event.preventDefault()
+var database = firebase.database();
 
-        trainName = $("#nameSearch").val().trim();
-        destination = $("#destinationSearch").val().trim();
-        firstTrain = $("#firstTrainSearch").val().trim();
-        frequency = $("#frequencySearch").val().trim();
+$("#submit").on("click", function () {
+    event.preventDefault()
 
-        database.ref().push({
-            name: trainName,
-            destination: destination,
-            firstTrain: firstTrain,
-            frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
+    trainName = $("#nameSearch").val().trim();
+    destination = $("#destinationSearch").val().trim();
+    firstTrain = $("#firstTrainSearch").val().trim();
+    frequency = $("#frequencySearch").val().trim();
 
+
+    database.ref().push({
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    database.ref().on("child_added", function (snapshot) {
-        
-        var sv = snapshot.val()
-        
+});
 
-        newDiv = $(`
+database.ref().on("child_added", function (snapshot) {
+
+    var sv = snapshot.val()
+
+    //trainTime pushed back a year so it comes before current time
+    var trainTimeCon = moment(sv.firstTrain, "HH:mm").subtract(1, "years")
+    console.log(trainTimeCon)
+
+    //sets current time to current time(moment)
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    //difference in current time v. train time converted in minutes
+    var diffTime = moment().diff(moment(trainTimeCon), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime)
+
+    //this gets the modulus remainder thing from the train frequency value??
+    var tRemainder = diffTime % sv.frequency;
+    console.log(tRemainder);
+
+    //minutes till the train comes calculation
+    var tMinutesTillTrain = sv.frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    //
+    var nextTrain = moment(moment().add(tMinutesTillTrain, "minutes")).format('HH:mm');
+    // console.log('ARRIVAL TIME: ' + moment(nextTrain).format('HH:mm'));
+
+
+
+    newDiv = $(`
         <tr>
-        <th> ${sv.name}</th>
+        <th> ${sv.name} </th>
         <th> ${sv.destination}</th>
-        <th> ${sv.firstTrain}</th>
-        <th></th>
-        <th> ${sv.frequency}</th>
-        
+        <th> ${"Every " + sv.frequency + " mins"}</th>
+        <th> ${nextTrain}</th>
+        <th> ${tMinutesTillTrain}</th>
         </tr>
             `)
-        
-        $("#list").append(newDiv)
 
-    })
+    $("#list").append(newDiv)
+
+})
+
+
+
